9f1f23ebeb93e2c5096d06768bebf4699c22d4a6
Marius Grondahl <marius@grondahldesign.no>
2019-07-01 17:38:20 +0200

    Added delete buttons

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,55 @@
-function deleteItem(e){
+// Lets calculate the sum of quantity and the price
+let productPrice = document.getElementsByClassName("product-price");
+let subTotal = document.getElementsByClassName("sub-total");
 
+
+function calcPrice () {
+    let quantity = document.getElementsByClassName("quantity");
+    let total = document.getElementById("total");
+    let totalArr = [];
+
+    for (let i = 0; i < quantity.length; i++) {
+        subTotal.value = quantity[i].value * productPrice[i].innerHTML;
+        subTotal[i].innerHTML = subTotal.value;
+        
+        totalArr.push(subTotal.value);
+    }
+
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    console.log(totalArr.reduce(reducer));
+
+    let totalSum = 0;
+    totalSum = totalArr.reduce(reducer);
+    
+    total.innerHTML = totalSum;
 }
 
-function getPriceByProduct(itemNode){
+ // Letss find the Parent-node of pressed button and delete it.
+let currentNode = "";
 
+function eventClick() { 
+  parentNode =  event.currentTarget.parentNode.parentNode.parentNode;
+  parentNodeChild = event.currentTarget.parentNode.parentNode;
+  parentNode.removeChild(parentNodeChild);
 }
 
-function updatePriceByProduct(productPrice, index){
+// Everything that needs to happen after Load
+window.onload = function() {
+  calcPrice();
 
-}
+  const calcButton = document.getElementById("calc-button");
+  calcButton.addEventListener('click', function (event) {
+    calcPrice();
+  });
 
-function getTotalPrice() {
 
-}
 
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+// here we add eventlistener to all deletebuttons
+  let deleteButtons = document.getElementsByClassName("btn-delete");
+  for (let j = 0; j < deleteButtons.length; j++) {
+      deleteButtons[j].addEventListener('click', function (event){
+        eventClick();
+    });
   }
+
 };
